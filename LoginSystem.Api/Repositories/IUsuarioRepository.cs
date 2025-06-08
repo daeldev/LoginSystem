43eb713ce763d4a9dc1152460596dbd022fb27db
daeldev <danielalmeida41263@gmail.com>
2025-06-08 00:05:03 -0300

    feat(api): implement UsuarioRepository with EF

--- LoginSystem.Api/Repositories/IUsuarioRepository.cs
+++ LoginSystem.Api/Repositories/IUsuarioRepository.cs
@@ -4,5 +4,10 @@ namespace LoginSystem.Api.Repositories
 {
     public interface IUsuarioRepository
     {
+        Task<IEnumerable<Usuario>> GetAllAsync();
+        Task<Usuario> GetByIdAsync(int id);
+        Task AddAsync(Usuario usuario);
+        Task UpdateAsync(Usuario usuario);
+        Task DeleteAsync(int id);
     }
 }
